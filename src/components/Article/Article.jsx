9343fc90d55572c9dc61683e33391da320668c0b
sithum-sathsara145 <sithumsathsara145@gmail.com>
2023-07-05 21:03:32 +0530

    Created blog component

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import './Article.css'
 
-export default function Article() {
+export default function Article({imgURL, title, date}) {
   return (
-    <div>Article</div>
+    <div className='gpt3__blog-container_article'>
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgURL} alt="blogImage" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full article</p>
+      </div>
+    </div>
   )
 }
